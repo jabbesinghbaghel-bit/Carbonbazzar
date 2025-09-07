e9972630500d8d48f368a1b13e27727411ff60c3
anshul <jabbesinghbaghel@gmail.com>
2025-09-07 13:27:09 +0530

    Connect API to MongoDB

--- pages/api/dbtest.js
+++ pages/api/dbtest.js
@@ -1,11 +1,29 @@
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
+let client;
+let clientPromise;
+
+if (!uri) {
+  throw new Error("Please define MONGODB_URI in Vercel Environment Variables");
+}
+
+if (process.env.NODE_ENV === "development") {
+  // Use a global variable in dev to prevent creating many clients
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  // New client for production
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
+}
 
 export default async function handler(req, res) {
   try {
-    const client = new MongoClient(uri);
-    await client.connect();
+    const client = await clientPromise;
     const db = client.db("carbonbazzar"); // database name
     const collections = await db.listCollections().toArray();
     res.status(200).json({ success: true, collections });
